a0f0649167a8ce5dc2ddb13e8ec9694b625135ef
JorG18 <jorgedrc18@gmail.com>
2026-06-09 20:06:04 +0200

    Add __version__ and GHCR Docker workflow

--- agentcrawl/__init__.py
+++ agentcrawl/__init__.py
@@ -1,11 +1,18 @@
 """AgentCrawl public API."""
 
+from importlib.metadata import PackageNotFoundError, version
+
 from .client import AgentCrawler
 from .config import CrawlConfig
 from .graphs import ExtractionGraph, MultiExtractionGraph, SearchGraph
 from .crawler import AgentCrawl
 from .models import CrawlResult, CrawlRun, MapResult, ScrapeDocument, SearchResult
 from .remote_client import AgentCrawlClient
+
+try:
+    __version__ = version("agentcrawl")
+except PackageNotFoundError:
+    __version__ = "0.1.0"
 
 __all__ = [
     "AgentCrawlClient",
